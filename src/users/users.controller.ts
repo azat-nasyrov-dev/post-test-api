4b7f391bd79b1de32c8200221ea6796a045e7cb9
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-01-27 01:24:29 +0600

    added registration logic

---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseInterface } from './types/user-response.interface';
+
+@Controller()
+export class UsersController {
+  private readonly logger = new Logger(UsersController.name);
+
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('users/register')
+  public async register(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+    const user = await this.usersService.signup(createUserDto);
+    return this.usersService.buildUserResponse(user);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { sign } from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
+import { UserResponseInterface } from './types/user-response.interface';
+
+@Injectable()
+export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  public async signup(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+
+    return await this.userRepository.save(newUser);
+  }
+
+  public generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      },
+      this.configService.get<string>('JWT_SECRET'),
+    );
+  }
+
+  public buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
+  }
+}
