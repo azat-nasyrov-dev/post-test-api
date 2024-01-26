478cde0b41e72c7f8c282a7235d3f7c43210796f
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-01-27 00:39:06 +0600

    added users migration

---
+++ src/migrations/1706294289520-CreateUsers.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUsers1706294289520 implements MigrationInterface {
+  name = 'CreateUsers1706294289520';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "username" character varying, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
+}
