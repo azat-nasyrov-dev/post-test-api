1c8c7d1862b943bf2f763103667dc4addb4b49ad
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-01-28 04:21:47 +0600

    added posts migration

---
+++ src/migrations/1706393993384-CreatePosts.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreatePosts1706393993384 implements MigrationInterface {
+  name = 'CreatePosts1706393993384';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "posts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "description" character varying NOT NULL DEFAULT '', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "posts"`);
+  }
+}
